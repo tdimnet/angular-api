e0ed3bcb17c388eb4c067ad9d0b6664b580a2f5a
tdimnet <thomas.dimnet@gmail.com>
2017-08-14 12:29:37 +0200

    Update get request headers

--- scripts/services/ratpApiService.js
+++ scripts/services/ratpApiService.js
@@ -5,8 +5,12 @@ angular
   .service('ratpApiService', function ($http) {
 
     this.getRatpInfos = function (callbackSuccess, callbackError) {
-      $http
-        .get('https://data.ratp.fr/explore/embed/dataset/liste-des-commerces-de-proximite-agrees-ratp/table/?disjunctive.code_postal&sort=code_postal&static=false&datasetcard=false')
+
+      var config = { headers:  {
+        'Access-Control-Allow-Origin': '*',
+      }
+    };
+      $http.get('https://data.ratp.fr/explore/embed/dataset/liste-des-commerces-de-proximite-agrees-ratp/table/?disjunctive.code_postal&sort=code_postal&static=false&datasetcard=false', config)
         .then(callbackSuccess)
         .catch(callbackError)
     }
